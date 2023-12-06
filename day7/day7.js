8177f1b694fe129e9a52d7e5f12431b259a7be49
Baptiste Trautmann <baptistetrautmann@hotmail.fr>
2023-12-06 17:16:48 +0100

    preparation for day 7

---
+++ day7/day7.js
@@ -0,0 +1,9 @@
+function resolveDay7(input) {
+    return undefined;
+}
+
+function resolveDay7Part2(input) {
+    return undefined;
+}
+
+export {resolveDay7, resolveDay7Part2}
---
+++ day7/day7.test.js
@@ -0,0 +1,32 @@
+import {content} from "../fileLoader.js";
+import {resolveDay7, resolveDay7Part2} from "./day7.js";
+/*
+test("Example result should be 288", () => {
+    const input = "";
+
+    expect(resolveDay7(input)).toBe(288);
+})
+
+test("should resolve day7 input ", async () => {
+
+    const text = await content('./day7/day7input.txt')
+
+    expect(resolveDay7(text)).toBe(741000);
+})
+
+test("Example result should be 71503 for part2", () => {
+    const input = ""+
+        "Time:      7  15   30\n" +
+        "Distance:  9  40  200";
+
+
+    expect(resolveDay7Part2(input)).toBe(71503);
+})
+
+
+test("should resolve day7 input part2 ", async () => {
+
+    const text = await content('./day7/day7input.txt')
+
+    expect(resolveDay7Part2(text)).toBe(38220708);
+})*/
