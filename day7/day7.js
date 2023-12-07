5a41bc2523b132b69bf16d4f8d32707c57ef8ec9
Baptiste Trautmann <baptistetrautmann@hotmail.fr>
2023-12-07 22:49:28 +0100

    solved day 7

--- day7/day7.js
+++ day7/day7.js
@@ -1,9 +1,246 @@
+function extractKind(hands, testFunction) {
+    return hands.filter(testFunction)
+}
+
+function uniqueChar(str) {
+    // Use a regex pattern to match unique characters
+    return [...new Set(str.match(/(.)(?!\1)/g))];
+}
+
+function isFiveOfAKind(hand) {
+    const handValue = hand.split(" ")[0];
+    return uniqueChar(handValue).length === 1
+}
+
+function isFourOfAKind(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 2) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 4;
+    }
+    return false;
+}
+
+function getIterationsOfCharsInHand(uniqueChars, handValue) {
+    return uniqueChars.map(value => {
+        const regex = new RegExp(value, "g");
+        return (handValue.match(regex) || []).length;
+    });
+}
+
+function isFullHouse(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 2) {
+        let charsValue = getIterationsOfCharsInHand(uniqueChars, handValue);
+        return (charsValue.includes(3) && charsValue.includes(2));
+    }
+    return false;
+}
+
+function getMaxIterationOfItemInHand(uniqueChars, handValue) {
+    return uniqueChars.reduce((accumulator, value) => {
+        const regex = new RegExp(value, "g");
+        return Math.max(accumulator, (handValue.match(regex) || []).length);
+    }, 0);
+}
+
+function isThreeOfAKind(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 3) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 3;
+    }
+    return false;
+}
+
+function isTwoPair(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 3) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 2;
+    }
+    return false;
+}
+
+function isOnePair(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 4) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 2;
+    }
+}
+
+function isHighCard(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    return uniqueChars.length === 5;
+}
+
+function compareCardValues(card1, card2) {
+    let valuesOrdered = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+    return valuesOrdered.indexOf(card1) > valuesOrdered.indexOf(card2) ? -1 : valuesOrdered.indexOf(card1) < valuesOrdered.indexOf(card2) ? 1 : 0;
+
+}
+
+function compareCardValuesWithJoker(card1, card2) {
+    let valuesOrdered = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
+
+    return valuesOrdered.indexOf(card1) > valuesOrdered.indexOf(card2) ? -1 : valuesOrdered.indexOf(card1) < valuesOrdered.indexOf(card2) ? 1 : 0;
+
+}
+
+function sortResults(hand1, hand2) {
+
+    for(let i = 0; i < 5; i++) {
+        const compareValue = compareCardValues(hand1.charAt(i), hand2.charAt(i));
+        if(compareValue !== 0){
+            return compareValue;
+        }
+    }
+
+    return 0;
+
+}
+
+function extractBid(hand) {
+    return parseInt(hand.split(" ")[1]);
+}
+
 function resolveDay7(input) {
-    return undefined;
+
+    let hands = input.split("\n");
+    
+    let fiveOfAKinds = extractKind(hands, isFiveOfAKind).sort(sortResults);
+    let fourOfAKinds = extractKind(hands, isFourOfAKind).sort(sortResults);
+    let fullHouses = extractKind(hands, isFullHouse).sort(sortResults);
+    let threeOfAKind = extractKind(hands, isThreeOfAKind).sort(sortResults);
+    let twoPair = extractKind(hands, isTwoPair).sort(sortResults);
+    let onePair = extractKind(hands, isOnePair).sort(sortResults);
+    let highCard = extractKind(hands, isHighCard).sort(sortResults);
+
+    const orderedHands = [...highCard,...onePair,...twoPair,...threeOfAKind,...fullHouses,...fourOfAKinds,...fiveOfAKinds];
+    let currentRank = 1;
+
+    return orderedHands.reduce((accumulator, hand) => {
+        return accumulator + extractBid(hand) * currentRank++;
+    },0)
+
+}
+
+function getJokerOccurrences(hand) {
+    return getMaxIterationOfItemInHand(['J'], hand);
+}
+
+function getMaxNonJokerOccurrences(uniqueChars, hand) {
+    uniqueChars.splice(uniqueChars.indexOf('J'),1);
+    return getMaxIterationOfItemInHand(uniqueChars, hand);
+}
+
+function isFiveOfAKindWithJoker(hand) {
+
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+
+    return uniqueChars.length === 1 || (uniqueChars.includes('J') && (getJokerOccurrences(handValue) + getMaxNonJokerOccurrences(uniqueChars, handValue)) === 5)
+}
+
+function isFourOfAKindWithJoker(hand) {
+
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 2 && !uniqueChars.includes('J')) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 4;
+    } else if(uniqueChars.includes('J')) {
+        return (getJokerOccurrences(handValue) + getMaxNonJokerOccurrences(uniqueChars, handValue)) === 4;
+    }
+    return false;
+}
+
+function isFullHouseWithJoker(hand) {
+
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    let charsValue = getIterationsOfCharsInHand(uniqueChars, handValue);
+    if(uniqueChars.length === 2 && !uniqueChars.includes('J')) {
+        return (charsValue.includes(3) && charsValue.includes(2));
+    } else if(getJokerOccurrences(handValue) === 1 && uniqueChars.length === 3) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 2;
+    }
+    return false;
+}
+
+function isThreeOfAKindWithJoker(hand) {
+
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 3 && !uniqueChars.includes('J')) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 3;
+    } else if(uniqueChars.includes('J') && uniqueChars.length !== 3) {
+        return (getJokerOccurrences(handValue) + getMaxNonJokerOccurrences(uniqueChars, handValue)) === 3;
+    }
+    return false;
+}
+
+
+function isTwoPairWithJoker(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 3 && !uniqueChars.includes('J')) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 2;
+    }
+    return false;
+}
+
+function isOnePairWithJoker(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    if(uniqueChars.length === 4 && !uniqueChars.includes('J')) {
+        return getMaxIterationOfItemInHand(uniqueChars, handValue) === 2;
+    } else if (uniqueChars.includes('J')){
+        return (getJokerOccurrences(handValue) + getMaxNonJokerOccurrences(uniqueChars, handValue)) === 2;
+    }
+    return false;
+}
+
+function isHighCardWithJoker(hand) {
+    const handValue = hand.split(" ")[0];
+    const uniqueChars = uniqueChar(handValue);
+    return uniqueChars.length === 5 && !uniqueChars.includes('J');
+}
+
+function sortResultsWithJoker(hand1, hand2) {
+
+    for(let i = 0; i < 5; i++) {
+        const compareValue = compareCardValuesWithJoker(hand1.charAt(i), hand2.charAt(i));
+        if(compareValue !== 0){
+            return compareValue;
+        }
+    }
+
+    return 0;
+
 }
 
 function resolveDay7Part2(input) {
-    return undefined;
+    let hands = input.split("\n");
+
+    let fiveOfAKinds = extractKind(hands, isFiveOfAKindWithJoker).sort(sortResultsWithJoker);
+    let fourOfAKinds = extractKind(hands, isFourOfAKindWithJoker).sort(sortResultsWithJoker);
+    let fullHouses = extractKind(hands, isFullHouseWithJoker).sort(sortResultsWithJoker);
+    let threeOfAKind = extractKind(hands, isThreeOfAKindWithJoker).sort(sortResultsWithJoker);
+    let twoPair = extractKind(hands, isTwoPairWithJoker).sort(sortResultsWithJoker);
+    let onePair = extractKind(hands, isOnePairWithJoker).sort(sortResultsWithJoker);
+    let highCard = extractKind(hands, isHighCardWithJoker).sort(sortResultsWithJoker);
+
+    const orderedHands = [...highCard,...onePair,...twoPair,...threeOfAKind,...fullHouses,...fourOfAKinds,...fiveOfAKinds];
+
+    let currentRank = 1;
+
+    return orderedHands.reduce((accumulator, hand) => {
+        return accumulator + extractBid(hand) * currentRank++;
+    },0)
 }
 
-export {resolveDay7, resolveDay7Part2}
+export {resolveDay7, resolveDay7Part2, isFiveOfAKind, isFourOfAKind, isFullHouse, isThreeOfAKind, isTwoPair, isOnePair, isHighCard, isFiveOfAKindWithJoker, isThreeOfAKindWithJoker}
--- day7/day7.test.js
+++ day7/day7.test.js
@@ -1,26 +1,104 @@
 import {content} from "../fileLoader.js";
-import {resolveDay7, resolveDay7Part2} from "./day7.js";
-/*
-test("Example result should be 288", () => {
-    const input = "";
+import {
+    isFiveOfAKind, isFiveOfAKindWithJoker,
+    isFourOfAKind,
+    isFullHouse, isHighCard, isOnePair,
+    isThreeOfAKind, isThreeOfAKindWithJoker,
+    isTwoPair,
+    resolveDay7,
+    resolveDay7Part2
+} from "./day7.js";
 
-    expect(resolveDay7(input)).toBe(288);
+test("Example result should be 6440", () => {
+    const input = "32T3K 765\n" +
+        "T55J5 684\n" +
+        "KK677 28\n" +
+        "KTJJT 220\n" +
+        "QQQJA 483";
+
+    expect(resolveDay7(input)).toBe(6440);
+})
+
+test("Should find fiveOfAKind in hand", () => {
+    const input ="AAAAA 684";
+
+    expect(isFiveOfAKind(input)).toBeTruthy();
+})
+
+test("Should find fourOfAKind in hand", () => {
+    const input ="AAAAT 684";
+
+    expect(isFourOfAKind(input)).toBeTruthy();
+})
+
+
+test("Should find fullHouse in hand", () => {
+    const input ="AAATT 684";
+
+    expect(isFullHouse(input)).toBeTruthy();
+})
+
+
+test("Should find threeOfAKind in hand", () => {
+    const input ="AAAT5 684";
+
+    expect(isThreeOfAKind(input)).toBeTruthy();
+})
+
+test("Should find twoPair in hand", () => {
+    const input ="ATAT5 684";
+
+    expect(isTwoPair(input)).toBeTruthy();
+})
+
+test("Should find onePair in hand", () => {
+    const input ="ATAK5 684";
+
+    expect(isOnePair(input)).toBeTruthy();
+})
+
+test("Should find highCard in hand", () => {
+    const input ="8JKTA 684";
+
+    expect(isHighCard(input)).toBeTruthy();
 })
 
 test("should resolve day7 input ", async () => {
 
     const text = await content('./day7/day7input.txt')
 
-    expect(resolveDay7(text)).toBe(741000);
+    expect(resolveDay7(text)).toBe(250957639);
 })
 
+
 test("Example result should be 71503 for part2", () => {
-    const input = ""+
-        "Time:      7  15   30\n" +
-        "Distance:  9  40  200";
+    const input = "32T3K 765\n" +
+        "T55J5 684\n" +
+        "KK677 28\n" +
+        "KTJJT 220\n" +
+        "QQQJA 483";
 
 
-    expect(resolveDay7Part2(input)).toBe(71503);
+    expect(resolveDay7Part2(input)).toBe(5905);
+})
+
+test("Should find fiveOfAKind with joker in hand", () => {
+    const input ="AAAAJ 684";
+
+    expect(isFiveOfAKindWithJoker(input)).toBeTruthy();
+})
+
+test("Should not be find fiveOfAKind with joker in hand", () => {
+    const input ="J49J4 548";
+
+    expect(isFiveOfAKindWithJoker(input)).toBeFalsy();
+})
+
+
+test("Should not be find threeOfAKind with joker in hand", () => {
+    const input ="2J299 282";
+
+    expect(isThreeOfAKindWithJoker(input)).toBeFalsy();
 })
 
 
@@ -28,5 +106,5 @@ test("should resolve day7 input part2 ", async () => {
 
     const text = await content('./day7/day7input.txt')
 
-    expect(resolveDay7Part2(text)).toBe(38220708);
-})*/
+    expect(resolveDay7Part2(text)).toBe(251515496);
+})
